b7e83a55e21094aaa0f8f30725944d1d390b59ae
randnamu <randnamu@163.com>
2024-09-17 11:20:57 +0800

    add mdx and expressive-code integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,23 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
 import UnoCSS from 'unocss/astro'
+import mdx from '@astrojs/mdx'
+import expressiveCode from 'astro-expressive-code'
+import { SITE } from './site.config'
 
 // https://astro.build/config
 export default defineConfig({
+  site: SITE.url,
   integrations: [
     UnoCSS({
       injectReset: true, // or a path to the reset file
     }),
+    expressiveCode({
+      themes: ['github-dark', 'github-light'],
+      defaultProps: {
+        wrap: true,
+      },
+    }),
+    mdx(),
   ],
 })
